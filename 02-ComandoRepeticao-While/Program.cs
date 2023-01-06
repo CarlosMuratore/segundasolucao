6dc57b73f93556c4a8773565cd525610d489a427
Carlos Muratore <carlosgustavo.muratore@gmail.com>
2023-01-05 22:45:39 -0300

    Segunda Solução - Aula 2

---
+++ 02-ComandoRepeticao-While/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02_ComandoRepeticao_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <=10)
+            {
+                Console.WriteLine($"Contando: {i}");
+                i++;
+            }
+            Console.WriteLine("--------------------");
+
+            for ( int j = 11; j <=20; j++)
+            {
+                Console.WriteLine($"Contando: {j}");
+            }
+            Console.WriteLine("--------------------");
+
+            int m = 0;
+            while (m <= 10)
+            {
+                Console.WriteLine($"{m} x 4 = {m * 4}");
+                m++;
+            }
+            Console.WriteLine("--------------------");
+
+            for (int n = 0; n <= 10; n++)
+            {
+                Console.WriteLine($"{n} x 6 = {n * 6}");
+            }
+
+
+        }
+    }
+}
---
+++ Exe01/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exe01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //quero cadastrar 2 alunos e suas 2 notas
+            //quero calcular a média das notas
+            //se média maior que 6 aprovado senão reprovado
+
+            string aluno1 = "Michael";
+            string aluno2 = "Andrea";
+
+            double Aluno1Nota1 = 6;
+            double Aluno1Nota2 = 7.5;
+            double Aluno2Nota1 = 5.5;
+            double Aluno2Nota2 = 6.5;
+
+            double mediaAluno1 = (Aluno1Nota1 + Aluno1Nota2) / 2;
+            double mediaAluno2 = (Aluno2Nota1 + Aluno2Nota2) / 2;
+
+            double media = 6;
+
+            if ( mediaAluno1 > media)
+            {
+                Console.WriteLine($"{aluno1} aprovado");
+            }
+            else
+            {
+                Console.WriteLine($"{aluno1} reprovado");
+            }
+            if (mediaAluno2 > media)
+            {
+                Console.WriteLine($"{aluno2} aprovado");
+            }
+            else
+            {
+                Console.WriteLine($"{aluno2} reprovado");
+            }
+        }
+    }
+}
